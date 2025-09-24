0122a70f15d7dc1d41be9662f384b6dda012f3aa
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-09-24 14:24:27 +0200

    refactor: migrate Header components to Tailwind CSS classes

--- refactor_react-vite-tailwind/src/components/ui/Header/HeaderDesktop.jsx
+++ refactor_react-vite-tailwind/src/components/ui/Header/HeaderDesktop.jsx
@@ -2,39 +2,66 @@ import ButtonR from "../Button/ButtonR";
 
 function HeaderDesktop() {
   return (
-    <header className="desktop">
-      <div className="wrapper">
-        <img className="logo" src="./images/logo.svg" alt="Easybank logo" />
-        <nav className="desktop-menu">
+    <header className='w-full h-[100px] fixed bg-white z-[3]'>
+      <div className='headerWrapper flex flex-row justify-between items-center h-full w-[85%] m-auto'>
+        <img
+          src='./images/logo.svg'
+          alt='Easybank logo'
+        />
+        <nav className='desktop-menu'>
           <ul>
             <li>
-              <a href="#hero" aria-label="Go to home section">
+              <a
+                href='#hero'
+                aria-label='Go to home section'
+                className='text-[#9698a6] p-[18px] xl:p-8 hover:border-b-4 hover:border-b-[#31d35c]'
+              >
                 Home
               </a>
             </li>
             <li>
-              <a href="#why-choose-us" aria-label="Go to why-choose-us section">
+              <a
+                href='#why-choose-us'
+                aria-label='Go to why-choose-us section'
+                className='text-[#9698a6] p-[18px] xl:p-8 hover:border-b-4 hover:border-b-[#31d35c]'
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#blog" aria-label="Go to Contact section">
+              <a
+                href='#blog'
+                aria-label='Go to Contact section'
+                className='text-[#9698a6] p-[18px] xl:p-8 hover:border-b-4 hover:border-b-[#31d35c]'
+              >
                 Contact
               </a>
             </li>
             <li>
-              <a href="#blog" aria-label="Go to Blog section">
+              <a
+                href='#blog'
+                aria-label='Go to Blog section'
+                className='text-[#9698a6] p-[18px] xl:p-8 hover:border-b-4 hover:border-b-[#31d35c]'
+              >
                 Blog
               </a>
             </li>
             <li>
-              <a href="#" aria-label="Go to Career section">
+              <a
+                href='#'
+                aria-label='Go to Career section'
+                className='text-[#9698a6] p-[18px] xl:p-8 hover:border-b-4 hover:border-b-[#31d35c]'
+              >
                 Careers
               </a>
             </li>
           </ul>
         </nav>
-        <ButtonR name={"Request Invite"} variant="gradient" type={"button"}></ButtonR>
+        <ButtonR
+          name={"Request Invite"}
+          variant='gradient'
+          type={"button"}
+        ></ButtonR>
       </div>
     </header>
   );
--- refactor_react-vite-tailwind/src/components/ui/Header/HeaderMobile.jsx
+++ refactor_react-vite-tailwind/src/components/ui/Header/HeaderMobile.jsx
@@ -5,10 +5,18 @@ function HeaderMobile({ isOpen, setOpen }) {
 
   return (
     <>
-      <header className="">
-        <div className="wrapper">
-          <img className="logo" src="/images/logo.svg" alt="Easybank logo" />
-          <img className="hamburger" src="/icons/icon-hamburger.svg" alt="Hamburger menu" onClick={toggleMenu} />
+      <header className='w-full h-[90px] fixed bg-white z-[3] '>
+        <div className='headerWrapper flex flex-row justify-between items-center h-full w-[85%] m-auto'>
+          <img
+            src='/images/logo.svg'
+            alt='Easybank logo'
+          />
+          <img
+            className='w-10'
+            src='/icons/icon-hamburger.svg'
+            alt='Hamburger menu'
+            onClick={toggleMenu}
+          />
         </div>
       </header>
     </>
